0cee053ed6564458fd01627e5e19efcc7f5db13b
Ronan Adriel Zenatti <pmboraceia@yahoo.com.br>
2018-10-17 16:39:59 -0300

    Classe Multiplicar

---
+++ CalculadoraOO/CalculadoraOO/Multiplicar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraOO
+{
+    class Multiplicar
+    {
+        public Multiplicar()
+        {
+            Console.WriteLine("Objeto Multiplicar criado.");
+        }
+
+        public Multiplicar(int num1, int num2)
+        {
+            int total = num1 * num2;
+            Console.WriteLine("Resultado da Mult: " + total);
+        }
+
+        public void MultEterno()
+        { 
+
+            
+        }
+    }
+}
---
+++ CalculadoraOO/CalculadoraOO/Soma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraOO
+{
+    class Soma
+    {
+        public Soma()
+        {
+            Console.WriteLine("Objeto Soma Criado.");
+        }
+    }
+}
